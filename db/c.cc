ea945b5916175f2f7b996951cd759ac39010cb52
Vincent Lee <cool8511@gmail.com>
2018-05-06 20:03:38 +0800

    get table property keys C API

--- db/c.cc
+++ db/c.cc
@@ -1128,6 +1128,62 @@ void rocksdb_approximate_sizes_cf(
   delete[] ranges;
 }
 
+void rocksdb_approximate_memtable_sizes(
+    rocksdb_t* db,
+    int num_ranges,
+    const char* const* range_start_key, const size_t* range_start_key_len,
+    const char* const* range_limit_key, const size_t* range_limit_key_len,
+    uint64_t* sizes) {
+  Range* ranges = new Range[num_ranges];
+  for (int i = 0; i < num_ranges; i++) {
+    ranges[i].start = Slice(range_start_key[i], range_start_key_len[i]);
+    ranges[i].limit = Slice(range_limit_key[i], range_limit_key_len[i]);
+    uint64_t count;
+    uint64_t size;
+    db->rep->GetApproximateMemTableStats(ranges[i], &count, &size);
+    sizes[i] += size;
+  }
+  delete[] ranges;
+}
+
+void rocksdb_approximate_memtable_sizes_cf(
+    rocksdb_t* db,
+    rocksdb_column_family_handle_t* column_family,
+    int num_ranges,
+    const char* const* range_start_key, const size_t* range_start_key_len,
+    const char* const* range_limit_key, const size_t* range_limit_key_len,
+    uint64_t* sizes) {
+  Range* ranges = new Range[num_ranges];
+  for (int i = 0; i < num_ranges; i++) {
+    ranges[i].start = Slice(range_start_key[i], range_start_key_len[i]);
+    ranges[i].limit = Slice(range_limit_key[i], range_limit_key_len[i]);
+    uint64_t count;
+    uint64_t size;
+    db->rep->GetApproximateMemTableStats(column_family->rep, ranges[i], &count, &size);
+    sizes[i] += size;
+  }
+  delete[] ranges;
+}
+
+uint64_t rocksdb_get_table_property_keynum_in_ranges(
+    rocksdb_t* db,
+    int num_ranges,
+    const char* const* range_start_key, const size_t* range_start_key_len,
+    const char* const* range_limit_key, const size_t* range_limit_key_len) {
+  Range* ranges = new Range[num_ranges];
+  for (int i = 0; i < num_ranges; i++) {
+    ranges[i].start = Slice(range_start_key[i], range_start_key_len[i]);
+    ranges[i].limit = Slice(range_limit_key[i], range_limit_key_len[i]);
+  }
+  rocksdb::TablePropertiesCollection props;
+  uint64_t total = 0;
+  db->rep->GetPropertiesOfTablesInRange(db->rep->DefaultColumnFamily(), ranges, num_ranges, &props);
+  for (const auto& n : props) {
+    total += n.second->num_entries;
+  }
+  return total;
+}
+
 void rocksdb_delete_file(
     rocksdb_t* db,
     const char* name) {
--- include/rocksdb/c.h
+++ include/rocksdb/c.h
@@ -351,6 +351,23 @@ extern ROCKSDB_LIBRARY_API void rocksdb_approximate_sizes_cf(
     const size_t* range_start_key_len, const char* const* range_limit_key,
     const size_t* range_limit_key_len, uint64_t* sizes);
 
+extern ROCKSDB_LIBRARY_API void rocksdb_approximate_memtable_sizes(
+    rocksdb_t* db, int num_ranges, const char* const* range_start_key,
+    const size_t* range_start_key_len, const char* const* range_limit_key,
+    const size_t* range_limit_key_len, uint64_t* sizes);
+
+extern ROCKSDB_LIBRARY_API void rocksdb_approximate_memtable_sizes_cf(
+    rocksdb_t* db, rocksdb_column_family_handle_t* column_family,
+    int num_ranges, const char* const* range_start_key,
+    const size_t* range_start_key_len, const char* const* range_limit_key,
+    const size_t* range_limit_key_len, uint64_t* sizes);
+
+extern ROCKSDB_LIBRARY_API uint64_t rocksdb_get_table_property_keynum_in_ranges(
+    rocksdb_t* db,
+    int num_ranges,
+    const char* const* range_start_key, const size_t* range_start_key_len,
+    const char* const* range_limit_key, const size_t* range_limit_key_len);
+
 extern ROCKSDB_LIBRARY_API void rocksdb_compact_range(rocksdb_t* db,
                                                       const char* start_key,
                                                       size_t start_key_len,
